58ba8d2abfbb9acf083712a193abd4fb5a98c87c
Dillon Nichols <ee.tinkerer@gmail.com>
2019-11-19 09:24:51 -0500

    get the correct tweets based on the webpage's year

--- projects/makevember/makevember.js
+++ projects/makevember/makevember.js
@@ -7,6 +7,14 @@ $('.date').hover(
   }
 );
 
+// get the current year from the page name
+// https://stackoverflow.com/a/30678857
+function getCurrentYear() {
+  var pathName = window.location.pathname;
+  var pageName = pathName.substring(pathName.lastIndexOf("/") + 1);
+  return pageName.substring(11, 15);
+}
+
 $('.date').click(function() {
   // remove old content:
   $("#tweet").empty();
@@ -16,8 +24,11 @@ $('.date').click(function() {
   // get date from clicked item
   var date = this.id;
 
+  // make the correct file based on the webpage's year
+  var filename = "makevember/tweets".concat(getCurrentYear()).concat(".json");
+
   // open the json file
-  $.getJSON("makevember/tweets2017.json", function(id) {
+  $.getJSON(filename, function(id) {
     // get the tweetID based on the date in the json
     var tweetID = id[date];
 
@@ -38,7 +49,7 @@ $('.date').click(function() {
   // scroll to tweet
   // https://stackoverflow.com/a/9348152/7564623
   $('html,body').animate({
-     scrollTop: $("#tweet").offset().top
+    scrollTop: $("#tweet").offset().top
   });
 
 });
